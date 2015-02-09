ee278905e50cf19d7fcfce091c3f18753902067f
Simon Taranto <simon.taranto@gmail.com>
2015-02-08 22:23:27 -0800

    dummy redirect

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+const (
+	defaultRedirectPath = "http://blog.simontaranto.com"
+)
+
+func main() {
+	http.HandleFunc("/", redirectHandler)
+
+	port := os.Getenv("port")
+	fmt.Printf("Running on port: %v", port)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Problem in ListenAndServe", err)
+	}
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	var redirectedPath string
+	requestedPath := r.URL.Path
+
+	rawRedirectedPath := urlMap()[requestedPath]
+
+	if rawRedirectedPath == "" {
+		redirectedPath = defaultRedirectPath
+	} else {
+		redirectedPath = defaultRedirectPath + rawRedirectedPath
+	}
+
+	http.Redirect(w, r, redirectedPath, 301)
+}
+
+func urlMap() map[string]string {
+	return map[string]string{
+		"/bingbingbing": "/bingbingbing",
+		"/foo":          "/bar",
+		"/zing":         "/zong",
+	}
 }
