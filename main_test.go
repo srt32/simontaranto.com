c7b9fd9721f09a8d6e4c3dc8e3f1d3a2fdc6f57b
Simon Taranto <simon.taranto@gmail.com>
2015-02-25 17:33:20 -0800

    Adds specs for redirect

---
+++ main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestUnknownURLRedirect(t *testing.T) {
+	requestedURL := "http://simontaranto.com/nonsense"
+	expectedURL := "http://blog.simontaranto.com"
+
+	handler := http.HandlerFunc(RedirectHandler)
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", requestedURL, nil)
+	if err != nil {
+		t.Errorf("error making request: %v", err)
+	}
+
+	handler.ServeHTTP(w, req)
+	if w.Code != 301 {
+		t.Errorf("Response was not 301, was: %v", w.Code)
+	}
+
+	actualURL := w.HeaderMap["Location"][0]
+	if actualURL != expectedURL {
+		t.Errorf("Redirected URL was not %v, was: %v", expectedURL, actualURL)
+	}
+}
+
+func TestKnownURLRedirect(t *testing.T) {
+	requestedURL := "http://simontaranto.com/2015/01/11/datetime-parsing-with-go.html"
+	expectedURL := "http://blog.simontaranto.com/post/2015-01-11-datetime-parsing-with-go.html"
+
+	handler := http.HandlerFunc(RedirectHandler)
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", requestedURL, nil)
+	if err != nil {
+		t.Errorf("error making request: %v", err)
+	}
+
+	handler.ServeHTTP(w, req)
+	if w.Code != 301 {
+		t.Errorf("Response was not 301, was: %v", w.Code)
+	}
+
+	actualURL := w.HeaderMap["Location"][0]
+	if actualURL != expectedURL {
+		t.Errorf("Redirected URL was not %v, was: %v", expectedURL, actualURL)
+	}
+}
